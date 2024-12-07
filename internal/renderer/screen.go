52b145401c26c6b3d556d896ae770312558ac4ce
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-08 02:48:53 +0300

    Screen initialization

--- internal/renderer/screen.go
+++ internal/renderer/screen.go
@@ -1,1 +1,12 @@
 package renderer
+
+import "sync"
+
+// Screen represents the terminal screen buffer
+type Screen struct {
+	mutex     sync.Mutex
+	width     uint8
+	height    uint8
+	colorMode ColorMode
+	buffer    [][]Cell
+}
