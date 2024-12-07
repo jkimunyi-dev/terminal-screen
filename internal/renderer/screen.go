37e2ae95624e084fce9cd6a90711feae814e6913
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-08 02:52:57 +0300

    NewScreen() initialise new screen with given dimensions and color

--- internal/renderer/screen.go
+++ internal/renderer/screen.go
@@ -11,9 +11,26 @@ type Screen struct {
 	buffer    [][]Cell
 }
 
+// Cell represents a single cell in the terminal screen
 type Cell struct {
 	Char      rune
 	FgColor   uint8
 	BgColor   uint8
 	Highlight bool
+}
+
+// NewScreen initializes a new screen with given dimensions and color mode
+func NewScreen(width, height uint8, mode ColorMode) *Screen {
+	screen := &Screen{
+		width:     width,
+		height:    height,
+		colorMode: mode,
+		buffer:    make([][]Cell, height),
+	}
+
+	for i := range screen.buffer {
+		screen.buffer[i] = make([]Cell, width)
+	}
+
+	return screen
 }
