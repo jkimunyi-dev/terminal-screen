8fe7d558ed205009101068350c788a7eeebe7bff
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-07 19:05:27 +0300

    screen setup command

--- internal/renderer/commands.go
+++ internal/renderer/commands.go
@@ -1,1 +1,9 @@
 package renderer
+
+// CommandType represents the different types of terminal rendering commands
+type CommandType uint8
+
+const (
+	// Screen Setup Command
+	CommandScreenSetup CommandType = 0x1
+)
