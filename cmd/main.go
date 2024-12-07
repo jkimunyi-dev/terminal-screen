c1c69d2172d5d25298de7c0d423d6e0e961393c8
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-12-08 02:58:45 +0300

    screen implementation

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jkimunyi-dev/termial-screen/internal/renderer"
+)
+
+func main() {
+	// Example of creating a screen
+	screen := renderer.NewScreen(80, 24, renderer.ColorMode16)
+
+	// Example of setting a cell
+	err := screen.SetCell(10, 5, renderer.Cell{
+		Char:    'H',
+		FgColor: 1, // Red foreground
+		BgColor: 0, // Black background
+	})
+
+	if err != nil {
+		log.Fatalf("Error setting cell: %v", err)
+	}
+
+	fmt.Println("Terminal Screen Renderer initialized successfully!")
+
+}
